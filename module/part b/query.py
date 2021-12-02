8935d5b78df9a95e86f95e83e0e905b385b9a09b
erict98 <tran.eric98@gmail.com>
2021-12-02 10:53:11 -0800

    updated query.py

--- module/part b/query.py
+++ module/part b/query.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import pandas
 import boto3
 import mysql.connector
 
@@ -17,26 +16,36 @@ os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
 # gets the credentials from .aws/certificate
 session = boto3.Session(profile_name='RDSCreds')
 client = session.client('rds')
-
-
 token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USR, Region=REGION)  
 
 """
-Connecting to DB instance
+Create a connection to the base
 """
 try:
-    conn = conn =  mysql.connector.connect(host=ENDPOINT, user=USR, passwd=token, port=PORT, database=DBNAME, ssl_ca='[full path]rds-combined-ca-bundle.pem')
-    cur = conn.cursor()                 # cursor object that assits with executing SQL queries
+    conn = mysql.connector.connect(host=ENDPOINT, 
+                                user=USR, 
+                                passwd=token,
+                                 port=PORT, 
+                                 database=DBNAME, 
+                                 ssl_ca='[full path]rds-combined-ca-bundle.pem')
+    cur = conn.cursor()
     cur.execute("""SELECT now()""")
-    query_results = cur.fetchall()
-    print(query_results)
+    results = cur.fetchall()
+    print(results)
 except Exception as e:
     print("Database connection failed due to {}".format(e))
 
 """
-Examples
-"""
+Example:
+Store the SQL statement into the query and execute the code with the cursor object
+Store the results from the cursor into results
 
+Note:
+SQL injections are common way for hackers to have unauthorized access to the database.
+Best practice dictates using safe query parameters instead of allowing the end-user to enter any SQL statement.
+"""
 query = "SELECT * FROM table"
-query_results = cur.fetchall();
+cur.execute(query)
+results = cur.fetchall()
+print(results)
 
